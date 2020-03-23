91466aa93888d30dc2c33961e6b6cbbcd1923a87
TooManyEggrolls <aphosch@gmail.com>
2020-03-23 18:55:30 -0500

    Version Updates

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pi-TIG
 Telegraf, InfluxDB, and Grafana
+
+# Sample Data
+    $ influx -precision rfc3339
+    Connected to http://localhost:8086 version 1.4.x
+    InfluxDB shell 1.4.x
+    > CREATE DATABASE NOAA_water_database
+    > exit
+
+    $ curl https://s3.amazonaws.com/noaa.water-database/NOAA_data.txt -o NOAA_data.txt
+    $ influx -import -path=NOAA_data.txt -precision=s -database=NOAA_water_database
+
+# Test Sample Data
+influx -precision rfc3339 -database NOAA_water_database
+
+# Graph Sample Data with Grafana
+Import 'NOAA Sample Data.json'
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Install InfluxDB as of 9/28/19 this is v.1.7.8
+#Install InfluxDB as of 3/22/20 this is v.1.7.10
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 echo "deb https://repos.influxdata.com/debian buster stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 sudo apt update &> /dev/null
@@ -8,13 +8,13 @@ sudo apt install influxdb -y &> /dev/null
 sudo systemctl enable influxdb
 sudo systemctl start influxdb
 
-#Install Telegraf as of 9/28/19 this is v.1.12.2
+#Install Telegraf as of 3/22/20 this is v.1.13.3
 sudo apt install telegraf -y &> /dev/null
 
-#Install Grafana 6.3.6 & dependencies 
+#Install Grafana 6.7.1 & dependencies
 sudo apt install fontconfig-config fonts-dejavu-core libfontconfig1 -y
-wget https://dl.grafana.com/oss/release/grafana_6.3.6_armhf.deb
-sudo dpkg -i grafana_6.3.6_armhf.deb
-rm grafana_6.3.6_armhf.deb
+wget https://dl.grafana.com/oss/release/grafana_6.7.1_armhf.deb
+sudo dpkg -i grafana_6.7.1_armhf.deb
+rm grafana_6.7.1_armhf.deb
 sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
